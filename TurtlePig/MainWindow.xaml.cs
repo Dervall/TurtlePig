31e9a8cf3b0d82db042d0c2cc4f217ba1e91be28
Per Dervall <pdervall@gmail.com>
2012-10-25 21:01:12 +0200

    Added expressions

--- TurtlePig/MainWindow.xaml.cs
+++ TurtlePig/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using Piglet.Lexer;
 using Piglet.Parser;
@@ -18,11 +19,45 @@ namespace TurtlePig
             {
                 canvas1.Children.Clear();
 
+                // Runtime stuff
                 var turtle = new Turtle(canvas1);
+                var variables = new Dictionary<string, int>();
 
+                // Parser configurator
                 var configurator = ParserFactory.Fluent();
                 var statementList = configurator.Rule();
                 var statement = configurator.Rule();
+                var variableDeclaration = configurator.Rule();
+                var expression = configurator.Rule();
+                var addSub = configurator.Rule();
+                var mulDiv = configurator.Rule();
+                var factor = configurator.Rule();
+
+                var variableIdentifier = configurator.Expression();
+                variableIdentifier.ThatMatches("$[A-Za-z0-9]+").AndReturns(f => f.Substring(1));
+
+                expression.IsMadeUp.By(addSub);
+
+                addSub.IsMadeUp.By(addSub).As("First").Followed.By("+").Followed.By(mulDiv).As("Second").WhenFound(f => f.First + f.Second)
+                    .Or.By(addSub).As("First").Followed.By("-").Followed.By(mulDiv).As("Second").WhenFound(f => f.First - f.Second)
+                    .Or.By(mulDiv);
+
+                mulDiv.IsMadeUp.By(mulDiv).As("First").Followed.By("*").Followed.By(factor).As("Second").WhenFound(f => f.First * f.Second)
+                    .Or.By(mulDiv).As("First").Followed.By("/").Followed.By(factor).As("Second").WhenFound(f => f.First / f.Second)
+                    .Or.By(factor);
+
+                factor.IsMadeUp.By<int>()
+                    .Or.By(variableIdentifier).As("Variable").WhenFound(f => variables[f.Variable])
+                    .Or.By("(").Followed.By(expression).As("Expression").Followed.By(")").WhenFound(f => f.Expression);
+
+                variableDeclaration.IsMadeUp.By("var")
+                    .Followed.By(variableIdentifier).As("Name")
+                    .Followed.By("=")
+                    .Followed.By(expression).As("InitialValue").WhenFound(f =>
+                    {
+                        variables.Add(f.Name, f.InitialValue);
+                        return null;
+                    });
 
                 statementList.IsMadeUp.ByListOf(statement);
                 statement.IsMadeUp.By("pendown").WhenFound(f =>
@@ -35,16 +70,17 @@ namespace TurtlePig
                         turtle.PenDown = false;
                         return null;
                     })
-                    .Or.By("move").Followed.By<int>().As("Distance").WhenFound(f =>
+                    .Or.By("move").Followed.By(expression).As("Distance").WhenFound(f =>
                     {
                         turtle.Move(f.Distance);
                         return null;
                     })
-                    .Or.By("rotate").Followed.By<int>().As("Angle").WhenFound(f =>
+                    .Or.By("rotate").Followed.By(expression).As("Angle").WhenFound(f =>
                     {
                         turtle.Rotate(f.Angle);
                         return null;
-                    });
+                    })
+                    .Or.By(variableDeclaration);
                 var parser = configurator.CreateParser();
                 parser.Parse(code.Text);    
             }
